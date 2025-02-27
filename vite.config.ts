d0ed9326481d2b2598ef95a872d4304034b83dc0
Chris Poulter <cpoulter@hotmail.co.uk>
2025-02-27 11:48:58 +0000

    add react scan

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig, loadEnv } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import reactScan from '@react-scan/vite-plugin-react-scan';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
 
     return {
-        plugins: [react(), tailwindcss()],
+        plugins: [react(), tailwindcss(), reactScan({ enable: true })],
         define: {
             'import.meta.env.npm_package_version': JSON.stringify(
                 env.npm_package_version
